643215845c84cd03a4de9b1c65046fbb02ec49b3
lanhao <lanhao@tik.com>
2018-07-07 15:13:27 +0800

    fix tpl

--- tpl/cronjob/main.js
+++ tpl/cronjob/main.js
@@ -1,19 +1,9 @@
 const schedule = require('node-schedule')
-const rate = require('./src/rate')
-const stock = require('./src/stock')
-const orderTimer = require('./src/order-timer')
+const demo = require('./src/demo')
 
-rate.start()
+
+demo.start()
 schedule.scheduleJob('0 0 */2 * * *', () => {
-  rate.start()
+  demo.start()
 })
 
-stock.start()
-schedule.scheduleJob('0 */1 * * * *', () => {
-  stock.start()
-})
-
-orderTimer.start()
-schedule.scheduleJob('0 */1 * * * *', ()=>{
-  orderTimer.start()
-})
