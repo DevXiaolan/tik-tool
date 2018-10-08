337229a5bf21fc0f285a4fb65625c2471bd5a1cd
lanhao <lanhao@tik.com>
2018-10-08 13:25:43 +0800

    release: 1.2.34

--- tpl/node/middlewares/logger.js
+++ tpl/node/middlewares/logger.js
@@ -22,8 +22,22 @@ module.exports = async (ctx, next) => {
     query: ctx.request.query,
     body: ctx.request.body
   });
+  const start = process.hrtime();
   await next();
+  const end = process.hrtime(start);
+  // 接口运行时间 ms
+  const t = (end[0] * 1e9 + end[1]) / 1e6;
+
+  // 如果时间大于阈值
+  if (t > (process.env.MAX_EXEC_TIME || 1000)) {
+    ctx.logger.error({
+      t,
+      traceId: ctx.state.traceId,
+      message: `接口执行时间太长: ${t} ms`
+    });
+  }
   ctx.logger.info({
+    t,
     type: 'OUT',
     status: ctx.response.status,
     message: ctx.response.message,
